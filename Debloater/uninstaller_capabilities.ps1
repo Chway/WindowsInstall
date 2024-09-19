f93693011a29b9ef63a683906f86f22f196b351d
Chway <20321127+Chway@users.noreply.github.com>
2024-09-19 14:14:28 +0200

    19092024_141428

--- Debloater/uninstaller_capabilities.ps1
+++ Debloater/uninstaller_capabilities.ps1
@@ -3,7 +3,12 @@ $removable = @("App.StepsRecorder~~~~0.0.1.0",
   "MathRecognizer~~~~0.0.1.0",
   "Media.WindowsMediaPlayer~~~~0.0.12.0",
   "Microsoft.Windows.WordPad~~~~0.0.1.0",
-  "OneCoreUAP.OneSync~~~~0.0.1.0")
+  "OneCoreUAP.OneSync~~~~0.0.1.0",
+  "Hello.Face.18967~~~~0.0.1.0",
+  "Language.TextToSpeech~~~en-GB~0.0.1.0",
+  "Language.Speech~~~en-GB~0.0.1.0",
+  "Language.OCR~~~en-GB~0.0.1.0",
+  "Language.Handwriting~~~en-GB~0.0.1.0")
 
 $capabilities = Get-WindowsCapability -Online | Where-Object State -EQ Installed | Select-Object Name
 
